a7eadfa8851d4626e43a80b6571a5f95fab936a9
tinomx <164075940+tinomx@users.noreply.github.com>
2024-04-07 22:25:08 -0600

    first commit

---
+++ MediLink/MediLink/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from myapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('', views.home_view, name='home'),
+    path('', include('django.contrib.auth.urls')),
+]
